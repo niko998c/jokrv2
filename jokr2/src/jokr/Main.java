535d9b3a1f55c793aab71cd719236fa16c45125d
niko998c <62287801+niko998c@users.noreply.github.com>
2020-08-28 10:49:45 +0200

    Add files via upload

---
+++ jokr2/src/jokr/Main.java
@@ -0,0 +1,42 @@
+package jokr;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Version 1.");
+        System.out.println("Hvad sagde blondinen, da hun gik ind i bussen?.. Av!");
+
+        System.out.println("Version 2.");
+
+        ArrayList<String> list = new ArrayList<String>();
+        String joke1 = "Hvad sagde Indiana Jones, da han skulle købe en bil?.. Harison Ford";
+        String joke2 = "Hvad kalder man et kriminelt får?.. En fårbryder";
+        String joke3 = "Hvad kalder man to kriminelle lamaer?.. Balademaer";
+        String joke4 = "Hvor mange babyer skal der til at male et hus? Det kommer an på hvor hårdt du kaster";
+        String joke5 = "Det ene skilt til det andet: Er du gift? Nej, jeg er skilt";
+        String joke6 = "Hvorfor stod der 17 blondiner foran natklubben?.. Man skulle være 18 for at komme ind";
+        String joke7 = "Hvad sagde blondinen, da hun gik ind i bussen?.. Av!";
+
+        list.add(joke1);
+        list.add(joke2);
+        list.add(joke3);
+        list.add(joke4);
+        list.add(joke5);
+        list.add(joke6);
+        list.add(joke7);
+
+        Scanner console = new Scanner(System.in);
+
+        String input = "";
+        while(true) {
+            int i = (int)Math.round(Math.random() * (list.size() -1));
+            input = console.nextLine();
+            if(input.equals("")) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
